490b1af41267bad04f0cdd496629dca466346540
Navya  Amble Chandrashekar <navyaac@Navyas-MacBook-Pro.local>
2018-02-01 21:41:52 -0800

    Execise1 commit1

--- Exercises/exercise_01.py
+++ Exercises/exercise_01.py
@@ -14,32 +14,48 @@ By the end of class on Thursday 1/25, students should have:
 
 def hello():
     """
-    Prints "Hello World"
-    :return: None
-    """
+        Prints "Hello World"
+        :return: None
+        """
+    print("Hello World")
     return
+
 
 def percent_decimal(i):
     """
-    Converts a percentage to a decimal or a decimal to a percentage depending on the input i
-    :param i: a float between 0 and 100
-    :return: a float between 0 and 100
-    """
-    return
+        Converts a percentage to a decimal or a decimal to a percentage depending on the input i
+        :param i: a float between 0 and 100
+        :return: a float between 0 and 100
+        """
+    if i > 1:
+        i = i / 100
+    else:
+        i = i*100
+    return i
+
 
 def exponent(integer, power):
     """
-    Using a loop (no imports!), raise the integer given to the power provided. (integer^power)
-    :param integer: a positive, non zero, integer
-    :param power: a positive, non zero, integer
-    :return: an integer
-    """
-    return
+        Using a loop (no imports!), raise the integer given to the power provided. (integer^power)
+        :param integer: a positive, non zero, integer
+        :param power: a positive, non zero, integer
+        :return: an integer
+        """
+    answer = 1
+    for _ in range(power):
+        answer *= integer
+    
+    return answer
+
 
 def complement(dna):
     """
-    Returns the complement strand of DNA to the input.  C <--> G,  A <--> T
-    :param dna: String containing only C, T, A, and G
-    :return: String containing only C, T, A, and G
-    """
-    return
+        Returns the complement strand of DNA to the input.  C <--> G,  A <--> T
+        :param dna: String containing only C, T, A, and G
+        :return: String containing only C, T, A, and G
+        """
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+    result = ''.join([complement[element] for element in dna[::1]])
+    
+    return result
+
